e242f7af197e4ee45575ccfa6e22610df8c2a12b
TheCodeStratagem <106943159+TheCodeStratagem@users.noreply.github.com>
2022-12-05 11:05:05 +0530

    Code cleanup and add comments

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
-
 from cs50 import SQL
 import datetime
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask import Flask, request, render_template, session, redirect, flash
-
-from flask import Flask
 from helpers import login_required, validate_password
+from werkzeug.security import check_password_hash, generate_password_hash
 
+# Constansts
+REVISION_SESSION_COUNT = 10
+
+# App setup
 app = Flask(__name__)
 
 app.jinja_env.filters["str"] = str
@@ -19,6 +19,7 @@ Session(app)
 
 db = SQL("sqlite:///timetable.db")
 
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -26,6 +27,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -43,7 +45,8 @@ def login():
             return redirect("/login")
 
         # Querying database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+                          request.form.get("username"))
 
         # len(rows) checks to see if username exists in database and check_password_hash() ensures password matches
         if len(rows) != 1 or not check_password_hash(rows[0]["password_hash"], request.form.get("password")):
@@ -106,10 +109,12 @@ def register():
                 return redirect("/register")
 
         # Generates hashed password to store in the database
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        hashed_password = generate_password_hash(
+            password, method='pbkdf2:sha256', salt_length=16)
 
         # Updates the database with the new values
-        db.execute("INSERT INTO users(username, password_hash) VALUES(?, ?)", username, hashed_password)
+        db.execute("INSERT INTO users(username, password_hash) VALUES(?, ?)",
+                   username, hashed_password)
 
         return render_template("login.html")
 
@@ -117,12 +122,16 @@ def register():
 @app.route("/")
 @login_required
 def index():
+    # Gets the user_id to query database
     user = session.get("user_id")
 
+    # Gets the subjects to display
     subjects = db.execute("SELECT * FROM subjects WHERE user_id = ?", user)
 
+    # Goes through each subject, and assigns a color based on proficiency.
+    # Level 1 is a dark red, while level 10 is a nice green
     for subject in subjects:
-        for i in range(10):
+        for i in range(REVISION_SESSION_COUNT):
             level = int(subject[f"proficiency_{i + 1}"])
             color = ""
             if level == 1:
@@ -148,20 +157,24 @@ def index():
             else:
                 color = "white"
 
+            # Assigns a color to the revision session
             subject[f'color_{i + 1}'] = color
 
-    return render_template("index.html", subjects=subjects)
+    # Data is rendered using jinja templates
+    return render_template("index.html", subjects=subjects, REVISION_SESSION_COUNT=REVISION_SESSION_COUNT)
 
 
 @app.route("/about")
 @login_required
 def about():
+    # Returns the about page
     return render_template("about.html")
 
 
 @app.route("/add", methods=["GET", "POST"])
 @login_required
 def add():
+    # Gets the user_id to query databases
     user = session.get("user_id")
 
     if request.method == "GET":
@@ -170,16 +183,19 @@ def add():
         subject = request.form.get("subject")
         subjects = db.execute("SELECT * FROM subjects WHERE user_id = ?", user)
 
+        # Gets a list of all the current subjects, and checks if the subject already exists
         subject_names = []
         for subject_name in subjects:
             subject_names.append(subject_name['subject'].lower())
 
-        if not subject or subject == "_" or subject == "" or subject.lower() in subject_names:
-            flash("Invalid subject/subject already in database")
+        if subject.lower() in subject_names:
+            flash("Subject already in database")
             return redirect("/")
 
+        # Logs the start date and adds the subject to the database
         today = datetime.datetime.today().strftime("%d/%m/%y")
-        db.execute("INSERT INTO subjects (user_id, subject, start_date) VALUES (?, ?, ?)", user, subject, today)
+        db.execute(
+            "INSERT INTO subjects (user_id, subject, start_date) VALUES (?, ?, ?)", user, subject, today)
         flash(f"Added subject: {subject}")
         return redirect("/")
 
@@ -187,6 +203,7 @@ def add():
 @app.route("/log", methods=["GET", "POST"])
 @login_required
 def log_revision():
+    # Gets the user_id to query databases
     user = session.get("user_id")
 
     if request.method == "GET":
@@ -195,29 +212,42 @@ def log_revision():
     else:
         log = request.form.get("revisionLog")
 
-        if log == "" or not log:
-            flash("Invalid Subject")
+        # Validates subject is in database
+        subjects = db.execute("SELECT * FROM subjects WHERE user_id = ?", user)
+        subject_names = []
+        for subject_name in subjects:
+            subject_names.append(subject_name['subject'].lower())
+
+        if log.lower() not in subject_names:
+            flash("Subject not in database")
             return redirect("/")
 
+        # Validates proficiency
         proficiency = int(request.form.get("proficiency"))
         if proficiency < 0 or proficiency > 10 or not proficiency or proficiency == "":
             flash("Invalid proficiency")
             return redirect("/")
 
+        # Gets the start date
         today = datetime.datetime.today().strftime("%d/%m/%y")
-        revision_num = int(db.execute("SELECT revision_num FROM subjects WHERE subject = ? AND user_id = ?", log, user)[0]['revision_num']) + 1
+
+        # Decides which revision session to update
+        revision_num = int(db.execute(
+            "SELECT revision_num FROM subjects WHERE subject = ? AND user_id = ?", log, user)[0]['revision_num']) + 1
 
         if revision_num == 10:
             flash("Can't update, table full!")
             return redirect("/")
         else:
-            db.execute(f"UPDATE subjects SET revision_{revision_num} = ?, proficiency_{revision_num} = ?, revision_num = ? WHERE subject = ? AND user_id = ?", today, proficiency, revision_num, log, user)
+            db.execute(
+                f"UPDATE subjects SET revision_{revision_num} = ?, proficiency_{revision_num} = ?, revision_num = ? WHERE subject = ? AND user_id = ?", today, proficiency, revision_num, log, user)
             return redirect("/")
 
 
 @app.route("/changepwd", methods=["GET", "POST"])
 @login_required
 def change_password():
+    # Gets the user_id to query databases
     user = session.get("user_id")
 
     if request.method == "GET":
@@ -231,14 +261,19 @@ def change_password():
             flash("Password and/or confirmation blank or do not match.")
             return redirect("/changepwd")
 
+        # Validates password to ensure password is decently secure
         if not validate_password(password):
             return redirect("/changepwd")
 
         # Generates hashed password to store in the database
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        hashed_password = generate_password_hash(
+            password, method='pbkdf2:sha256', salt_length=16)
 
-        db.execute("UPDATE users SET password_hash = ? WHERE id = ?", hashed_password, user)
+        # Updates the database
+        db.execute("UPDATE users SET password_hash = ? WHERE id = ?",
+                   hashed_password, user)
 
+        # Logs user out and then displays login page.
         logout()
         return redirect("/")
 
@@ -246,6 +281,7 @@ def change_password():
 @app.route("/timer", methods=["GET", "POST"])
 @login_required
 def timer():
+    # Displays timer page, which is run by Javascript
     if request.method == "GET":
         return render_template("timer.html")
 
@@ -253,6 +289,7 @@ def timer():
 @app.route("/delete", methods=["GET", "POST"])
 @login_required
 def delete():
+    # Gets the user_id and subjects to query databases
     user = session.get("user_id")
     subjects = db.execute("SELECT * FROM subjects WHERE user_id = ?", user)
 
@@ -261,15 +298,18 @@ def delete():
     else:
         subject = request.form.get("subject")
 
+        # Validates subject exists
         subject_names = []
         for subject_name in subjects:
             subject_names.append(subject_name['subject'])
 
-        if not subject or subject == "_" or subject == "" or subject not in subject_names:
+        if subject not in subject_names:
             flash("Invalid subject")
             return redirect("/")
 
-        db.execute("DELETE FROM subjects WHERE subject = ? AND user_id = ?", subject, user)
+        # Updates database to delete subject
+        db.execute(
+            "DELETE FROM subjects WHERE subject = ? AND user_id = ?", subject, user)
         flash("Deleted successfully")
         return redirect("/")
 
@@ -277,15 +317,19 @@ def delete():
 @app.route("/reset", methods=["POST"])
 @login_required
 def reset_timetable():
+    # Gets user_id to query databases
     user = session.get("user_id")
 
+    # Deletes all subject records with matching user_id
     db.execute("DELETE FROM subjects WHERE user_id = ?", user)
     flash("Deleted timetable succesfully")
     return redirect("/")
 
+
 @app.route("/resetsubj", methods=["POST"])
 @login_required
 def reset_subject():
+    # Gets the user_id to query databases, subjects to validate subject
     user = session.get("user_id")
     subjects = db.execute("SELECT * FROM subjects WHERE user_id = ?", user)
     subject = request.form.get("resetSubject")
@@ -294,12 +338,20 @@ def reset_subject():
     for subject_name in subjects:
         subject_names.append(subject_name['subject'])
 
-    if not subject or subject == "_" or subject == "" or subject not in subject_names:
+    # Checks to see if a valid subject was submitted
+    if subject not in subject_names:
         flash("Invalid subject")
         return redirect("/")
 
-    db.execute("DELETE FROM subjects WHERE user_id = ? AND subject = ?", user, subject)
+    # Deletes subject from database
+    db.execute(
+        "DELETE FROM subjects WHERE user_id = ? AND subject = ?", user, subject)
+
+    # Gets the start date
     today = datetime.datetime.today().strftime("%d/%m/%y")
-    db.execute("INSERT INTO subjects (user_id, subject, start_date) VALUES (?, ?, ?)", user, subject, today)
+
+    # Adds the subject to the database again
+    db.execute(
+        "INSERT INTO subjects (user_id, subject, start_date) VALUES (?, ?, ?)", user, subject, today)
     flash(f"Reset {subject} succesfully")
     return redirect("/")
